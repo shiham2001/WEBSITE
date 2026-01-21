b0e71814c68ef56472b57aca7ebe5801f0fc0f81
Mohammed Shiham <mohammedshiham3@gmail.com>
2026-01-21 13:24:51 +0530

    more2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let music = document.getElementById("bgMusic");
+let isPlaying = false;
+
+function toggleMusic() {
+    if (isPlaying) {
+        music.pause();
+    } else {
+        music.play();
+    }
+    isPlaying = !isPlaying;
+}
+
 function openStory(text) {
     document.getElementById("story").innerText = text;
     document.getElementById("popup").style.display = "block";
